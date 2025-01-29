4c6a825f00642fcfa7f43f7d9131e321a5184624
김동환 <forwarder1121@naver.com>
2025-01-29 21:45:07 +0900

    Denpendency Injection

--- 0301-training-di/src/test/java/com/example/training/service/TrainingServiceImplTest.java
+++ 0301-training-di/src/test/java/com/example/training/service/TrainingServiceImplTest.java
@@ -2,16 +2,20 @@ package com.example.training.service;
 
 import java.util.List;
 
+import com.example.training.repository.TrainingRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.example.training.entity.Training;
+import org.mockito.Mockito;
 
 class TrainingServiceImplTest {
     @Test
     public void test_findAll() {
-        TrainingService trainingService = null;
-
+        TrainingRepository trainingRepository = new MockTrainingRepository();
+        TrainingService trainingService = new TrainingServiceImpl(trainingRepository);
+        // test도 하나의 "외부" 코드이고
+        // repository를 만들어서, serviece 객체에 DI
         @SuppressWarnings("null")
 		List<Training> trainings = trainingService.findAll();
         // 결과 확인
